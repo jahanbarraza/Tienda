170ebcc7927eb628a98587ae53123dd029c80027
Jahan Barraza <jahanyu@gmail.com>
2025-01-23 11:16:58 -0500

    Despliegue

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -21,7 +21,7 @@ const options = {
 }
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola mi server en express');
 });
 
